0770122bbb42243c68578afa4b2dbd90237a925a
ARTHUR Van <93868542+BeyondTheKeys@users.noreply.github.com>
2021-12-13 22:54:01 +0000

    Add db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const encryption = require('./encryption')
 const app = express();
-
+const db = require("quick.db")
 
 app.get('/', (req, res) => {
   res.type('html')
@@ -10,16 +10,16 @@ app.get('/', (req, res) => {
 app.get('/api/connect', (req, res) => {
   res.send("hi2")
 });
-
+app.get('/api/start', (req, res) => {
+  res.send("hi2")
+});
 app.get('/api/connect/:num', (req, res) => {
   const num = req.params.num
    const key = encryption.encrypt(JSON.stringify({hi: "hi"}), num)
   res.json({ connectionKey: key, id: num, test: JSON.parse(encryption.decrypt(key, num))})
-  
-  
 });
 
 module.exports = {
-  start: app.listen(3000)
+  start: function() {app.listen(3000)}
 }
 
